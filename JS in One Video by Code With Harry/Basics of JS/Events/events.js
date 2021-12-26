da8bed7a5345b21460548225366c7857a4d5fd32
nikhil051102 <nikhildeore05@gmail.com>
2021-12-27 00:06:28 +0530

    -

---
+++ JS in One Video by Code With Harry/Basics of JS/Events/events.js
@@ -0,0 +1,25 @@
+// Generally Events in Javascript are used to trigger actions in browser or to tracking activity by user. 
+// function clicked() {
+//     console.log("clicked");
+// };
+
+// window.onload = function() {        // window.onload is a special event that is triggered when the page is loaded.
+//     console.log("window loaded");
+// };
+
+// button.addEventListener('click', function()  // addEventListener is a method of the DOM element that is used to add an event listener to the element.
+// {
+//     console.log("Button pe Click kia gaya hai");
+// });
+
+// Paragraph1.addEventListener('mouseover', function()  // addEventListener is a method of the DOM element that is used to add an event listener to the element.
+// {
+//     console.log("Mouse is over Paragraph1");
+// });
+
+
+
+
+
+// Set Timeout and Set Interval
+// Arrow Functions
