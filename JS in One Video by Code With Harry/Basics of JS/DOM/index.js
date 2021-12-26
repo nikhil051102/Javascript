f6247508f88d3a95149863927241685d7b696662
nikhil051102 <nikhildeore05@gmail.com>
2021-12-26 09:22:56 +0530

    -

---
+++ JS in One Video by Code With Harry/Basics of JS/DOM/index.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById('button'))  //Get the button element
+
+let elem = document.getElementsByClassName('content');
+console.log(elem);
+elem[0].style.background = 'orange';
+
+
